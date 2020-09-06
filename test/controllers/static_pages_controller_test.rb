1371995af845f30c9aeb99ee26d200fb482dd2d7
Inamine Towa <pank24ever@outlook.jp>
2020-09-06 14:44:44 +0900

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,8 +1,9 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
+
+  test "should get root" do
+    get root_url
     assert_response :success
   end
 
@@ -11,4 +12,8 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+  end
 end
